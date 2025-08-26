a5ba901a569a02dced92a2faf70e420d1a2a98be
Saad Arqam <saadarqam@Saads-MacBook-Pro.local>
2025-08-26 12:59:03 +0530

    added custom fonts and audios

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <Navbar />
-    <div className="text-4xl font-bold text-center">SocialLifts</div>
+    {/* <div className="text-4xl font-bold text-center">SocialLifts</div> */}
     <Contact />
   </>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,21 +3,21 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="bg-gray-100 p-4 rounded text-black">
-      <h1>Contact Us</h1>
+      <h1 className='text-red-400 text-2xl font-bold'>Contact Us</h1>
       <form>
         <label>
           Name:
-          <input type="text" name="name" />
-        </label>
+          <input className='border p-2 rounded' type="text" name="name" />
+        </label><br />
         <label>
           Email:
-          <input type="email" name="email" />
-        </label>
+          <input className='border p-2 rounded' type="email" name="email" />
+        </label><br />
         <label>
           Message:
-          <textarea name="message" />
-        </label>
-        <button type="submit">Submit</button>
+          <textarea className='border p-2 rounded' name="message" />
+        </label><br />
+        <button className='bg-blue-500 text-white p-2 rounded hover:cursor-pointer' type="submit">Submit</button>
       </form>
     </div>
   )
