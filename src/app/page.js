c37289cdb7628ff928beee71b9b533be523da7c4
Saad Arqam <saadarqam@Saads-MacBook-Pro.local>
2025-08-26 11:53:47 +0530

    added contact page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Navbar from '../components/Navbar';
-
+import Contact from  '../components/Contact';
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <div className="text-4xl font-bold text-center">SocialLifts</div>
-    </>
+    <Contact />
+  </>
   );
 }
