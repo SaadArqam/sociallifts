1b30a73878d88a74c4e1723805593475c1c2f449
Saad Arqam <saadarqam@Saads-MacBook-Pro.local>
2025-08-26 22:29:36 +0530

    added audio

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar';
 import Contact from  '../components/Contact';
+import './globals.css';
 
 export default function Home() {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,19 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="bg-gray-100 p-4 rounded text-black">
-      <h1 className='text-red-400 text-2xl font-bold'>Contact Us</h1>
-      <form>
+      <h1 className='text-red-400 text-9xl font-bold font-coign text-center'>Contact Us</h1>
+      <form className='flex flex-col space-y-4 text-center'>
         <label>
           Name:
-          <input className='border p-2 rounded' type="text" name="name" />
+          <input className='border p-2 rounded' type="text" name="name" placeholder='Enter your full name...'/>
         </label><br />
         <label>
           Email:
-          <input className='border p-2 rounded' type="email" name="email" />
+          <input className='border p-2 rounded' type="email" name="email" placeholder='Enter your email...'/>
         </label><br />
         <label>
           Message:
-          <textarea className='border p-2 rounded' name="message" />
+          <textarea className='border p-2 rounded' name="message" placeholder='Enter your message...' />
         </label><br />
         <button className='bg-blue-500 text-white p-2 rounded hover:cursor-pointer' type="submit">Submit</button>
       </form>
